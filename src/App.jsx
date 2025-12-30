6f7c10e34b0668716dcd31101a0497f03cec9006
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-12-30 22:15:05 +0300

    Replace alert with console log for persisted auth in App component

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,7 @@ const DiaryPage = lazy(() => import('./pages/DiaryPage'));
 function App() {
   const dispatch = useDispatch();
   const isRefreshing = useSelector(selectIsRefreshing);
-  alert(localStorage.getItem('persist:auth'));
-
+  console.log(localStorage.getItem('persist:auth'));
   useEffect(() => {
     const persistedAuth = localStorage.getItem('persist:auth');
     console.log(persistedAuth);
