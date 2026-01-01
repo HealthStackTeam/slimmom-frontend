8bf7fcad5ab0b3fb36f714780eb66f6807f9367e
Fatih Genç <fatihgenc_61@outlook.com>
2026-01-01 19:42:03 +0300

    tarih düzeltme2

--- src/pages/CalculatorPage.jsx
+++ src/pages/CalculatorPage.jsx
@@ -9,7 +9,7 @@ const CalculatorPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const getTodayDate = () => {
     const today = new Date();
-    return `${String(today.getDate()).padStart(2, '0')}.${String(today.getMonth() + 1).padStart(2, '0')}.${today.getFullYear()}`;
+    return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
   };
   const openModal = () => {
     setIsModalOpen(true);
