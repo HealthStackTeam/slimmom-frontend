42418f14743832b5c67bf7cebcac270fb69cff9e
ozncncyr <ozncncyr@outlook.com>
2026-01-06 21:51:32 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,19 +1,41 @@
-import { Link } from "react-router-dom";
-import logoDesktop from "../../assets/logo-SlimMom.png";
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
+import { useTheme } from '../../context/ThemeContext';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import styles from './Logo.module.css';
+
+import logoDesktop from '../../assets/logo-SlimMom.png';
 import logoMobile from '../../assets/Logo-mobile.svg';
-import styles from "./Logo.module.css";
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
-import { useMediaQuery } from "react-responsive";
+import logoDesktopDark from '../../assets/logo-SlimMom-DarkTheme.png';
+import logoMobileDark from '../../assets/logo-mobile-DarkTheme.png';
 
 const Logo = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isMobile = useMediaQuery({ maxWidth: 767 });
+  const { theme } = useTheme();
+
+  const isDark = theme === 'dark';
+
+  let currentLogo;
+  let logoClass;
+
+  if (isLoggedIn) {
+    currentLogo = isDark ? logoDesktopDark : logoDesktop;
+    logoClass = styles.logoIcon;
+  } else {
+    if (isMobile) {
+      currentLogo = isDark ? logoMobileDark : logoMobile;
+      logoClass = styles.logoIconMobile;
+    } else {
+      currentLogo = isDark ? logoDesktopDark : logoDesktop;
+      logoClass = styles.logoIcon;
+    }
+  }
+
   return (
     <Link to="/" className={styles.logoWrapper}>
-      <img src={isLoggedIn
-        ? logoDesktop
-        : (isMobile ? logoMobile : logoDesktop)} alt="SlimMom Logo" className={styles.logoIcon} />
+      <img src={currentLogo} alt="SlimMom Logo" className={logoClass} />
     </Link>
   );
 };
--- src/components/UserInfo/UserInfoMobile.jsx
+++ src/components/UserInfo/UserInfoMobile.jsx
@@ -3,11 +3,11 @@ import UserInfo from '../UserInfo/UserInfo';
 import styles from './UserInfoMobile.module.css';
 
 const UserInfoMobile = ({ isLoggedIn }) => {
-  return (
+  return isLoggedIn ? (
     <div className={styles.userInfoMobileWrapper}>
       <UserInfo isLoggedIn={isLoggedIn} />
     </div>
-  );
+  ) : null;
 };
 
 export default UserInfoMobile;
