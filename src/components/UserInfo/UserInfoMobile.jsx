c241ea091a7f0796923b3a7b5615df32dfd3b8d7
ozncncyr <ozncncyr@outlook.com>
2026-01-06 23:35:19 +0300

    style: update CSS for responsive design and layout adjustments

--- src/components/UserInfo/UserInfoMobile.jsx
+++ src/components/UserInfo/UserInfoMobile.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import UserInfo from '../UserInfo/UserInfo';
 import styles from './UserInfoMobile.module.css';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
-const UserInfoMobile = ({ isLoggedIn }) => {
+const UserInfoMobile = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return isLoggedIn ? (
-    <div className={styles.userInfoMobileWrapper}>
-      <UserInfo isLoggedIn={isLoggedIn} />
+    <div className={styles.userInfoMobile}>
+      <div className={styles.userInfoMobileInner}>
+        {' '}
+        <UserInfo />
+      </div>
     </div>
   ) : null;
 };
